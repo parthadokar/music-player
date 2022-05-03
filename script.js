d182d03fb47ea4871cddeba51e6580ee78b01d46
Parth Adokar <adokar.parth@gmail.com>
2022-05-03 16:35:08 +0530

    new-page

--- script.js
+++ script.js
@@ -12,89 +12,167 @@ const currTime = document.querySelector('#currTime');
 const durTime = document.querySelector('#durTime');
 
 // Song titles
+const songs = ['Wildest Dreams', 'Cardigan', 'Lover','Exile'];
 
-const songs = ['Wildest Dreams','Lover','Exile','Cardigan'];
+// Keep track of song
+let songIndex = 2;
 
-// Keep track of songs
-
-let songIndex = 0;
-
-// Initially load song info DOM
-
+// Initially load song details into DOM
 loadSong(songs[songIndex]);
 
 // Update song details
-
 function loadSong(song) {
-    title.innerText = song;
-    audio.src = `music/${song}.mp3`;
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
 }
 
+// Play song
 function playSong() {
-    musicContainer.classList.add('play');
-    playBtn.querySelector('i.fas').classList.remove('fa-play');
-    playBtn.querySelector('i.fas').classList.add('fa-play');
-    audio.play();
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play();
 }
 
+// Pause song
 function pauseSong() {
-    musicContainer.classList.remove('play');
-    playBtn.querySelector('i.fas').classList.add('fa-play');
-    playBtn.querySelector('i.fas').classList.remove('fa-play');
-    audio.pause();
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+  audio.pause();
 }
 
+// Previous song
 function prevSong() {
-    songIndex--;
-    if(songIndex < 0) {
-        songIndex = songs.length - 1;
-    }
+  songIndex--;
 
-    loadSong(songs[songIndex]);
-    playSong();
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
 }
 
+// Next song
 function nextSong() {
-    songIndex++;
-    if(songIndex > songs.length) {
-        songIndex = 0;
-    }
+  songIndex++;
 
-    loadSong(songs[songIndex]);
-    playSong();
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
 }
 
+// Update progress bar
 function updateProgress(e) {
-    const {duration,currentTime} = e.srcElement;
-    const progressPercent = (currentTime / duration) * 100;
-    progress.style.width = `${progressPercent}%`;
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
 }
 
+// Set progress bar
 function setProgress(e) {
-    const width = this.clientWidth;
-    const clickX = e.offsetX;
-    const duration = audio.duration;
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
 
-    audio.currentTime = (clickX / width) * duration;
+  audio.currentTime = (clickX / width) * duration;
 }
-// Event Listeners
 
-playBtn.addEventListener('click',() => {
-    const isPlaying = musicContainer.classList.contains('play');
+//get duration & currentTime for Time of song
+function DurTime (e) {
+	const {duration,currentTime} = e.srcElement;
+	var sec;
+	var sec_d;
 
-    if(isPlaying) {
-        pauseSong();
-    } else {
-        playSong();
-    }
-})
+	// define minutes currentTime
+	let min = (currentTime==null)? 0:
+	 Math.floor(currentTime/60);
+	 min = min <10 ? '0'+min:min;
 
-// Change song events
+	// define seconds currentTime
+	function get_sec (x) {
+		if(Math.floor(x) >= 60){
+			
+			for (var i = 1; i<=60; i++){
+				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+					sec = Math.floor(x) - (60*i);
+					sec = sec <10 ? '0'+sec:sec;
+				}
+			}
+		}else{
+		 	sec = Math.floor(x);
+		 	sec = sec <10 ? '0'+sec:sec;
+		 }
+	} 
 
+	get_sec (currentTime,sec);
+
+	// change currentTime DOM
+	currTime.innerHTML = min +':'+ sec;
+
+	// define minutes duration
+	let min_d = (isNaN(duration) === true)? '0':
+		Math.floor(duration/60);
+	 min_d = min_d <10 ? '0'+min_d:min_d;
+
+
+	 function get_sec_d (x) {
+		if(Math.floor(x) >= 60){
+			
+			for (var i = 1; i<=60; i++){
+				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+					sec_d = Math.floor(x) - (60*i);
+					sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+				}
+			}
+		}else{
+		 	sec_d = (isNaN(duration) === true)? '0':
+		 	Math.floor(x);
+		 	sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+		 }
+	} 
+
+	// define seconds duration
+	
+	get_sec_d (duration);
+
+	// change duration DOM
+	durTime.innerHTML = min_d +':'+ sec_d;
+		
+};
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+// Change song
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 
-audio.addEventListener('timeupdate',updateProgress);
-progressContainer.addEventListener('click', setProgress)
+// Time/song update
+audio.addEventListener('timeupdate', updateProgress);
 
-audio.addEventListener('ended',nextSong);
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
+
+// Time of song
+audio.addEventListener('timeupdate',DurTime);
